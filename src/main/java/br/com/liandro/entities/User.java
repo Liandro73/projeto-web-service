7430ef05e5b93d8a57209d8d9d2331406fac423e
Dernival Liandro <liandro.sys@gmail.com>
2020-04-09 18:07:49 -0300

    User entity and resource

---
+++ src/main/java/br/com/liandro/entities/User.java
@@ -0,0 +1,20 @@
+package br.com.liandro.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+}
