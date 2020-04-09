9c67b0187b84a9009d8c5c1e7fc5dc369621a667
Dernival Liandro <liandro.sys@gmail.com>
2020-04-09 16:12:07 -0300

    Project created

---
+++ src/main/java/br/com/liandro/ProjetoWebServiceApp.java
@@ -0,0 +1,13 @@
+package br.com.liandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoWebServiceApp.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/liandro/ProjetoWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.liandro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
