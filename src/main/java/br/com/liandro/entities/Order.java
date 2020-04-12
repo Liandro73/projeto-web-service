c03bfd01108ddd6618b8653a6029219c06d8e81b
Dernival Liandro <liandro.sys@gmail.com>
2020-04-12 00:56:13 -0300

    OrderStatus enum

--- src/main/java/br/com/liandro/entities/Order.java
+++ src/main/java/br/com/liandro/entities/Order.java
@@ -1,9 +1,8 @@
 package br.com.liandro.entities;
 
+import br.com.liandro.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -11,8 +10,6 @@ import java.time.Instant;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "tb_order")
 public class Order implements Serializable {
 
@@ -25,8 +22,28 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
 
 }
